9c3e9f76d786b2fc071f142c448adffec8c85d3e
Foteno <patrikass32@gmail.com>
2024-03-29 09:00:44 +0200

    minimal example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {NggModule} from "@sebgroup/green-angular";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NggModule
   ],
   providers: [],
   bootstrap: [AppComponent]
